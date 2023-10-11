a8db4fdf255c534e76e4f7b8663a303c8d897d7c
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-10-11 23:20:49 +0700

    Create homapageData.js

---
+++ src/assets/data/homapageData.js
@@ -0,0 +1,185 @@
+const bestSale = [
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$80.00"
+    },
+    {
+        img : "",
+        classification : "CONCENTRATES",
+        name : "Mix And Match Shatter/Budder 28g (4 X 7G)",
+        rate : "4.6/5",
+        review : 212,
+        compound : "Sativa 100%",
+        price : "$102.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Ahi Tuna + Master Tuna",
+        rate : "4.6/5",
+        review : 135,
+        compound : "Sativa 100%",
+        price : "$120.00"
+    }
+]
+
+const testimonials = [
+    {
+        avatar : "",
+        name : "Vikki Starr",
+        rate : 4,
+        comment : "Absolutely love TopShelfBC; affordable on any budget and such fast delivery, straight to my door! I recommend them to all my friends and family for their 420 needs."
+    },
+    {
+        avatar : "",
+        name : "Terry Baskey",
+        rate : 5,
+        comment : "Best damn place to buy your canabis at great prices"
+    },
+    {
+        avatar : "",
+        name : "Teran Buskai",
+        rate : 3,
+        comment : "Best damn place to buy your canabis at great prices"
+    }
+]
+
+const choose = [
+    {
+        img : "",
+        classification : "CONCENTRATES",
+        name : "Mix And Match Shatter/Budder 28g (4 X 7G)",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Indica 70%",
+        price : "$102.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$80.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$80.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Ahi Tuna + Master Tuna",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$120.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$80.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Ahi Tuna + Master Tuna",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$120.00"
+    },
+    {
+        img : "",
+        classification : "CONCENTRATES",
+        name : "Mix And Match Shatter/Budder 28g (4 X 7G)",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Indica 70%",
+        price : "$102.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$80.00"
+    }
+]
+
+const recently = [
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$80.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Ahi Tuna + Master Tuna",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$120.00"
+    },
+    {
+        img : "",
+        classification : "CONCENTRATES",
+        name : "Mix And Match Shatter/Budder 28g (4 X 7G)",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$102.00"
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : "$80.00"
+    }
+]
+
+const education = [
+    {
+        img : "",
+        date: "January 24, 2023",
+        title: "12 Mistakes To Avoid When Buying Cannabis Online",
+        content: "Buying cannabis online can be a great way to get your hands on the products you need without leaving the comfort of your home. But …"
+    },
+    {
+        img : "",
+        date: "January 20, 2023",
+        title: "How To Store Cannabis and Keep it Fresh and Potent?",
+        content: "Cannabis packaging has advanced dramatically in recent years, whether your state has a medicinal marijuana programme, legal adult-use weed, or both. Most ..."
+    },
+    {
+        img : "",
+        date: "January 19, 2023",
+        title: "The Ultimate Guide to Checking the Quality of Cannabis – 10 Industry Leading Tips",
+        content: "Quality cannabis is a term used to describe cannabis products that meet specific standards of excellence. It is essential to understand what quality cannabis means, …"
+    }
+]
