35ed21d0ddeaad50382d19da557fe96570cc40c7
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-10-25 03:11:33 +0700

    create productpage data

---
+++ src/assets/data/productPageData.js
@@ -0,0 +1,64 @@
+const product ={
+    img : "",
+    classification : "FLOWER",
+    name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+    rate : "4.6/5",
+    review : 132,
+    compound : "Sativa 100%",
+    price : 80
+}
+const testimonials = [
+    {
+        avatar : "",
+        name : "Vikki Starr",
+        rate : 4,
+        comment : "Absolutely love TopShelfBC; affordable on any budget and such fast delivery, straight to my door! I recommend them to all my friends and family for their 420 needs.",
+        date : "January 15, 2023"
+    },
+    {
+        avatar : "",
+        name : "Terry Baskey",
+        rate : 5,
+        comment : "Best damn place to buy your canabis at great prices",
+        date : "January 5, 2023"
+    }
+]
+
+const featured = [
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : 80
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Ahi Tuna + Master Tuna",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : 120
+    },
+    {
+        img : "",
+        classification : "CONCENTRATES",
+        name : "Mix And Match Shatter/Budder 28g (4 X 7G)",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Indica 100%",
+        price : 102
+    },
+    {
+        img : "",
+        classification : "FLOWER",
+        name : "2 Oz Deal Watermelon Zkittles + Purple Gushers",
+        rate : "4.6/5",
+        review : 132,
+        compound : "Sativa 100%",
+        price : 80
+    }
+]
